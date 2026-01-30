8cb2e5e725b88b75eb576b85ef19c63bc0a7e63f
david.parra.cruz <69942426+parrrra@users.noreply.github.com>
2026-01-30 17:51:24 +0100

    Solo se puede editar si no se ha visitado

--- src/app/features/timeline/place-item.component.ts
+++ src/app/features/timeline/place-item.component.ts
@@ -53,15 +53,16 @@ import { CommonModule } from '@angular/common';
           </a>
         }
 
-        <button
-  class="text-gray-400 hover:text-gray-700 cursor-pointer"
-  (click)="edit.emit(place)"
->
-  <i class="fa-solid fa-pen"></i>
-</button>
-
-
         @if (!place.visited) {
+
+                    <button
+            class="text-gray-400 hover:text-gray-700 cursor-pointer"
+            (click)="edit.emit(place)"
+            >
+            <i class="fa-solid fa-pen"></i>
+        </button>
+
+
           <button
             class="text-gray-400 hover:text-red-500 cursor-pointer"
             (click)="delete.emit(place.id)"
